27290c70591b6dc6171e45c9a4fd97bcfe753fd1
Pavlo Shabak <properfraction66@yahoo.com>
2023-04-28 17:33:00 +0300

    Add login functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Nunito } from 'next/font/google';
 import Navbar from './components/navbar/Navbar';
 import RegisterModal from './components/modals/RegisterModal';
 import ToasterProvider from './providers/ToasterProvider';
+import LoginModal from './components/modals/LoginModal';
+import getCurrentUser from './actions/getCurrentUser';
 
 const nunito = Nunito({ subsets: ['latin'] });
 
@@ -12,17 +14,20 @@ export const metadata = {
   description: 'Airbnb clone.',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang='en'>
       <body className={nunito.className}>
         <ToasterProvider />
         <RegisterModal />
-        <Navbar />
+        <LoginModal />
+        <Navbar currentUser={currentUser} />
         {children}
       </body>
     </html>
