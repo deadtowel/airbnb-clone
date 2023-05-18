ec0c4e2c67f2b64fd1d22fcd437ae11a3b6bf6d6
Pavlo Shabak <properfraction66@yahoo.com>
2023-05-19 01:17:57 +0300

    Add functionality for reservation

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,6 +1,6 @@
 import getCurrentUser from '@/app/actions/getCurrentUser';
 import getListingById from '@/app/actions/getListingById';
-//import getReservations from '@/app/actions/getReservations';
+import getReservations from '@/app/actions/getReservations';
 
 import EmptyState from '@/app/components/EmptyState';
 
@@ -12,8 +12,7 @@ interface IParams {
 
 const ListingPage = async ({ params }: { params: IParams }) => {
   const listing = await getListingById(params);
-  //const reservations = await getReservations(params);
-  const reservations = undefined;
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
